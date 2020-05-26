e8d7ac01e53b063f8f0760a4841fbc07744edcd8
Alex Hamilton <alexhami916@gmail.com>
2020-05-26 15:54:12 -0600

    Added path to mysql server to my computer so that I could run the schema and seeds files through the terminal. Minor adjustments made to db files.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- SEEDS.SQL 
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheese", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
