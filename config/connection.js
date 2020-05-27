7b84b0009481702dd36387bad472a8aea273a224
Alex Hamilton <alexhami916@gmail.com>
2020-05-27 13:33:39 -0600

    Setup config directory.

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+///////////////////////
+//// CONNECTION.JS ////
+///////////////////////
+
+// Importing mysql node package
+var mysql = require("mysql");
+
+// Used to create connection to database
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "root",
+	database: "burgers_db"
+});
+
+// Initialize node connection
+connection.connect(function(err) {
+	if(err) {
+		console.error("Connection failure. " + err.stack);
+		return;
+	}
+	console.log("mySQL connection, ID: " + connection.threadId);
+});
+
+// Exporting connection for application use
+module.exports = connection;
---
+++ config/orm.js
@@ -0,0 +1,25 @@
+//////////////////
+///// ORM.JS /////
+//////////////////
+
+var connection = require("./connection");
+
+
+var selectAll = function() {
+
+};
+
+var insertOne = function() {
+    
+};
+
+var updateOne = function() {
+    
+};
+
+
+module.exports = {
+    selectAll = selectAll,
+    insertOne = insertOne,
+    updateOne = updateOne
+};
